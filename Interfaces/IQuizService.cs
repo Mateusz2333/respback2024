81d4f79b3212801736034f510068797e5d9b3498
Mateusz2333 <150080629+Mateusz2333@users.noreply.github.com>
2024-03-11 23:04:32 +0100

    Add files via upload

---
+++ Interfaces/IQuizService.cs
@@ -0,0 +1,11 @@
+﻿using BackendLab01;
+using System.Collections.Generic;
+
+namespace ApplicationCore.Interfaces
+{
+    public interface IQuizService
+    {
+        List<Quiz> GetAllQuizzes();
+        Quiz GetQuizById(int id);
+    }
+}
